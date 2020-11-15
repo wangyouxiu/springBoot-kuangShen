b12cd6709ed51d6feddf560cf844eb76b4d1db45
wangyue <wangyue01@zto.cn>
2020-11-16 01:04:41 +0800

    feat: 增加druid过滤器

--- src/main/java/com/kuang/config/DruidConfig.java
+++ src/main/java/com/kuang/config/DruidConfig.java
@@ -2,11 +2,14 @@ package com.kuang.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.servlet.Filter;
 import javax.sql.DataSource;
 import java.util.HashMap;
 
@@ -42,6 +45,23 @@ public class DruidConfig {
         return bean;
     }
 
+    //filter
+    @Bean
+    public FilterRegistrationBean webStatFilter(){
+        FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>();
+
+        bean.setFilter(new WebStatFilter());
+
+        //可以过滤哪些请求呢？
+        HashMap<String, String> initParameters = new HashMap<>();
+
+        //这些东西不进行统计
+        initParameters.put("exclusions", "*.js,*.css,/druid/*");
+
+        bean.setInitParameters(initParameters);
+
+        return bean;
+    }
 
 
 
